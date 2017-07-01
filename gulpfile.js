a2c020178390dea2e654102a23757326988ab952
joshua <jwells.asi@gmail.com>
2017-06-30 23:32:09 -0400

    gulp cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,6 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug')
 
-gulp.task('partial', function(){
-  return gulp.src('view/partials/*.pug')
-    .pipe(pug({
-    	pretty: true
-    }))
-    .pipe(gulp.dest('build'))   
-});
-
 gulp.task('pages', function(){    
   return gulp.src('view/pages/*.pug')
     .pipe(pug({
@@ -20,5 +12,4 @@ gulp.task('pages', function(){
 
 gulp.task('watch', function(){
   gulp.watch('view/pages/*.pug', ['pages']);
-  gulp.watch('view/partials/*.pug', ['partial']);
 })
