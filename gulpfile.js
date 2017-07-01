2a6710d0444738cf37741ac4a685d7eb5d76ba1e
joshua <jwells.asi@gmail.com>
2017-06-30 22:36:37 -0400

    implement gulp watch to aide in recompiling after save

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 
 gulp.task('partial', function(){
+  gulp.watch('view/partial/*.pug', ['partial']);
   return gulp.src('view/partial/*.pug')
     .pipe(pug({
     	pretty: true
@@ -11,6 +12,7 @@ gulp.task('partial', function(){
 });
 
 gulp.task('pages', function(){
+  gulp.watch('view/pages/*.pug', ['pages']);    
   return gulp.src('view/pages/*.pug')
     .pipe(pug({
     	pretty: true
