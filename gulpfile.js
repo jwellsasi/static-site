cb8cc846121eaa3fe276f56cba65e2bd0a1b60aa
joshua <jwells.asi@gmail.com>
2017-06-30 23:24:22 -0400

    gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,25 @@
 // run with: gulp default
 var gulp = require('gulp');
-var pug = require('gulp-pug');
+var pug = require('gulp-pug')
 
 gulp.task('partial', function(){
-  gulp.watch('view/partial/*.pug', ['partial']);
-  return gulp.src('view/partial/*.pug')
+  return gulp.src('view/partials/*.pug')
     .pipe(pug({
     	pretty: true
     }))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build'))   
 });
 
-gulp.task('pages', function(){
-  gulp.watch('view/pages/*.pug', ['pages']);    
+gulp.task('pages', function(){    
   return gulp.src('view/pages/*.pug')
     .pipe(pug({
     	pretty: true
     }))  
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build'))    
 });
 
-gulp.task('default', ['partial', 'pages']);
+gulp.task('watch', function(){
+  gulp.watch('view/pages/*.pug', ['pages']);
+  gulp.watch('view/partials/*.pug', ['partial']);
+  // Other watchers
+})
