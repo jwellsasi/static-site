30c4c8f6293fb25456aa966dda04dcbbd75a0cf1
joshua <jwells.asi@gmail.com>
2017-07-01 11:37:28 -0400

    gulp will now start node when it finishes building the templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 // run with: gulp default
 var gulp = require('gulp');
 var pug = require('gulp-pug')
+var exec =  require('child_process').exec;
 
-gulp.task('pages', function(){    
+// gulp task that will re-compile 
+// the templates to html
+gulp.task('pages', function(){  
   return gulp.src('view/pages/*.pug')
     .pipe(pug({
     	pretty: true
@@ -10,6 +13,19 @@ gulp.task('pages', function(){
     .pipe(gulp.dest('build'))    
 });
 
-gulp.task('watch', function(){
-  gulp.watch('view/pages/*.pug', ['pages']);
+// start the node server
+gulp.task('server', function (cb) {
+  exec('node app.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 })
+
+// watch for changes in the template files
+// then restart the server
+gulp.task('watch', function(){
+  gulp.watch('view/pages/*.pug', ['pages','server']); 
+})
+
+gulp.task('default', ['pages', 'watch', 'server'])
